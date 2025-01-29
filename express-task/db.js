afa66fdd07fcdc9a71863538948236e34ad94240
DHARUN-PRAKASH <dharunprakash2904@gmail.com>
2025-01-29 14:55:50 +0530

    first commit

---
+++ express-task/db.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb+srv://dharunprakash:12345@cluster0.tttb0r3.mongodb.net/microservices?retryWrites=true&w=majority&appName=Cluster0");
